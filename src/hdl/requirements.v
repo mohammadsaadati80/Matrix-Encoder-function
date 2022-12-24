49ddfec9a7ac2205565dd0fc8d2b0aed7fe06823
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2022-12-24 18:18:21 +0330

    Add some modules

---
+++ src/hdl/requirements.v
@@ -0,0 +1,100 @@
+module register (clk,pin,ld,rst,pout);
+    
+    parameter N = 25;
+    input clk;
+    input ld;
+    input rst;
+    input [N-1:0]pin;
+    output reg [N-1:0]pout;
+
+    always @(posedge clk) begin
+        if(rst) pout <= 0;
+        else pout <= pin;
+    end
+
+endmodule
+
+module counter (clk,pin,select,ld,rst,en,pout,co);
+    
+    parameter N = 8;
+    input clk;
+    input ld;
+    input en;
+    input select;
+    input rst;
+    input [N-1:0]pin;
+    output co;
+    output reg [N-1:0]pout;
+
+    always @(posedge clk) begin
+        if(rst) pout <= 0;
+        else if(ld) pout <= pin;
+        else begin
+            if(en)begin
+                if(select == 1) pout <= pout + 1;
+                if(select == 0) pout <= pout - 1;
+            end
+        end
+    end
+
+    assign co = (select == 1) ? &{pout} : &{~pout};
+
+endmodule
+
+module shift_register (clk,pin,select,cin,ld,rst,en,pout);
+    
+    parameter N = 25;
+    input clk;
+    input ld;
+    input en;
+    input select;
+    input cin;
+    input rst;
+    input [N-1:0]pin;
+    output reg [N-1:0]pout;
+
+    always @(posedge clk) begin
+        if(rst) pout <= 0;
+        else if(ld) pout <= pin;
+        else begin
+            if(en)begin
+                if(select == 1) pout <= {pout[N-2:0],cin};
+                if(select == 0) pout <= {cin,pout[N-1:1]};
+            end
+        end
+    end
+
+endmodule
+
+module mux2to1 (a,b,s,w);
+
+    parameter N = 8;
+    input [N-1:0]a;
+    input [N-1:0]b;
+    input s;
+    output [N-1:0]w;
+
+    assign w = (s==1'b0) ? a : b; 
+    
+endmodule
+
+module mux4to1 (a,b,c,d,s,w);
+
+    parameter N = 8;
+    input [N-1:0]a;
+    input [N-1:0]b;
+    input [N-1:0]c;
+    input [N-1:0]d;
+    input [1:0]s;
+    output reg [N-1:0]w;
+
+    always @(*) begin
+        case (s)
+            0 : w = a; 
+            1 : w = b; 
+            2 : w = c; 
+            3 : w = d; 
+        endcase
+    end
+    
+endmodule
