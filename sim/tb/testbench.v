bf6ff6fa9486e12bce63df2f9fdac5799d502b28
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2022-12-28 11:18:09 +0330

    Update testbench

--- sim/tb/testbench.v
+++ sim/tb/testbench.v
@@ -4,14 +4,14 @@ module TB()
 
     reg clk = 0, rst = 0, start = 0;
 
-    permutation_func test(clk, rst, start, "input.txt", "output.txt");
+    permutation_func test(.clk(clk), .rst(rst), .start(start), .input_file_name("input.txt"), .output_file_name("output.txt"));
 
     always #10 clk = ~clk;
     initial begin
         rst = 1;
         #10 rst = 0;
         #10 start = 1;
-        #10 start = 0;
+        #10000 start = 0;
         #10000 $finish;
 
     end
