ff764909bc6f4a8b73d258b7b5f71bcd40c813f1
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2023-01-01 17:48:06 +0330

    Modularity

---
+++ src/hdl/controller.v
@@ -0,0 +1,85 @@
+module controller (
+	start, counter_64_co, rst, clk, write_en , read_en, mux_en,
+	reg_en , cnt_64_en, done, reg_rst, permute_en
+);
+
+	input start;
+	input counter_64_co;
+	input rst;
+	input clk;
+	output reg permute_en;
+	output reg write_en; 
+	output reg read_en; 
+	output reg mux_en;
+	output reg reg_en;
+	output reg cnt_64_en;
+	output reg done;
+	output reg reg_rst;
+
+	reg [2:0] ps , ns ;
+	parameter [2:0] Idle = 0 , Beginn = 1 , Read = 2 , PassInput = 3, Swap = 4 , PassOutput = 5, Write = 6; 
+	reg check_start;
+
+
+	assign check_start = (counter_64_co) ? 0 : start ;
+
+	always@(posedge clk , posedge rst) begin
+		if (rst == 1'b1)
+			ps <= Idle;
+		else
+			ps <= ns;
+	end
+
+	always@(ps, start, counter_64_co) begin
+		ns = Idle ;
+		case (ps)
+			Idle:
+				ns = (check_start) ? Beginn : Idle;
+			Beginn:
+				ns = Read;
+			Read:
+				ns = PassInput;
+			PassInput:
+				ns = Swap;
+			Swap:
+				ns = PassOutput ;
+			PassOutput:
+				ns = Write;
+			Write:
+				ns = (counter_64_co) ? Idle : Beginn;
+			default :
+				ns = Idle;
+		endcase
+	end
+
+	always@(ps , counter_64_co) begin
+		write_en = 1'b0 ; reg_en = 1'b0 ; cnt_64_en = 1'b0; reg_rst = 1'b0; permute_en = 1'b0; read_en = 1'b0; mux_en = 1'b0;
+		done = 1'b0; 
+		case (ps)
+		Idle: begin
+			reg_rst = 1'b1;
+		end
+		Beginn : begin
+			done = (counter_64_co) ? 1'b1 : 1'b0;
+		end
+		Read : begin
+			read_en = 1'b1;
+		end
+		PassInput : begin
+			reg_en = 1'b1;
+		end
+		Swap : begin
+			permute_en = 1'b1;
+		end
+		PassOutput : begin
+			reg_en = 1'b1;
+			mux_en = 1'b1;
+		end
+		Write : begin
+			cnt_64_en = 1'b1;
+			write_en = 1'b1;
+		end
+		endcase
+	end
+
+endmodule
--- src/hdl/requirements.v
+++ src/hdl/requirements.v
@@ -41,22 +41,6 @@ module counter (clk,pin,select,ld,rst,en,pout,co);
 
 endmodule
 
-module read_from_file (clk, en, line_in, line);
-
-    // input string input_file_name;
-    input [24:0] line_in;
-    input clk;
-    input en;
-    output reg [24:0] line;
-	
-	always@(en, clk) begin
-		if (en) begin
-			line <= line_in;
-		end
-	end
-
-endmodule
-
 module write_to_file ( output_file_name, en, line);
 
     // input string output_file_name;
@@ -92,38 +76,4 @@ module mux2to1 (a,b,s,w);
     
 endmodule
 
-module swap (input_line, swap_en, output_line);
 
-    input [24:0] input_line;
-    input swap_en;
-    output reg [24:0] output_line;
-
-    reg [24:0] temp;
-
-    integer i = 0;
-    integer x = 0;
-    integer y = 0;
-    integer new_x = 0;
-    integer new_y = 0;
-    integer new_pos = 0;
-
-    always @(swap_en) begin
-        if (swap_en) begin
-            for(i = 0; i < 25; i = i + 1) begin
-                x = i % 5;
-                y = i / 5;
-                x = (x + 3) % 5;
-                y = (y + 3) % 5;
-                new_x = y;
-                new_y = (2 * x + 3 * y) % 5;
-                new_x = (new_x + 2) % 5;
-                new_y = (new_y + 2) % 5;
-                new_pos = (new_y * 5) + new_x;
-                temp[new_pos] = input_line[i];
-            end
-        end
-    end
-
-    assign output_line = temp;
-
-endmodule
