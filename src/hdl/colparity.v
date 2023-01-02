55ae8d7ff3e93ad5ab95b5e68d8568a7f266a9b9
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2023-01-02 18:59:48 +0330

    Update colParity

--- src/hdl/colparity.v
+++ src/hdl/colparity.v
@@ -6,26 +6,28 @@ module colParity (input1, input2, output_line);
 
     reg [24:0] temp;
 
-    genvar i = 0;
+    genvar i;
     integer x = 0;
     integer y = 0;
-    integer new_x = 0;
-    integer new_y = 0;
-    integer new_pos = 0;
+    wire a_ijk = 1'b0;
+    wire parity1 = 1'b0;
+    wire parity2 = 1'b0;
 
     generate
         for(i = 0; i < 25; i = i + 1) begin
-                x = i % 5;
-                y = i / 5;
-                x = (x + 3) % 5;
-                y = (y + 3) % 5;
-                
-                a = input2[x][y];
+            assign x = i % 5;
+            assign y = i / 5;
+            assign x = (x + 3) % 5;
+            assign y = (y + 3) % 5;
 
-
-
-                new_pos = (y * 5) + x;
-                temp[new_pos] = input_line[i];
+            assign a_ijk = input2[i];
+            assign parity1 = input2[(i+14)%25] ^ input2[(i+19)%25] ^ input2[(i+24)%25] ^ input2[(i+4)%25] ^ input2[(i+9)%25];
+            assign parity1 = input1[(i+16)%25] ^ input1[(i+21)%25] ^ input1[(i+1)%25] ^ input1[(i+6)%25] ^ input1[(i+11)%25];
+            
+            // x = (x + 2) % 5;
+            // y = (y + 2) % 5;
+            // pos = (y * 5) + x;
+            assign temp[i] = a_ijk ^ parity1 ^ parity2;
         end
     endgenerate
 
