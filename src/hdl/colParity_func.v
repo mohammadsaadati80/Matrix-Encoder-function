ae7ac5246c44d89de92cd0188199264696c44cbf
mamadi80 <mm_araghi80@yahoo.com>
2023-01-02 21:00:39 +0330

    fix func

--- src/hdl/colParity_func.v
+++ src/hdl/colParity_func.v
@@ -22,7 +22,7 @@ module colParity_func (clk, rst, start, donee, cnt_value, line_in, write_enable,
 	assign write_value = write_val;
 
 	controller cntrl( .start(start), .cnt_co(counter_64_co), .rst(rst), .clk(clk), .wr_en(write_en),
-	.inreg_en(reg_en), .cnt_en(cnt_64_en), .done(done), .cnt_rst(rst) );
+	.inreg_en(reg_en), .cnt_en(cnt_64_en), .done(done), .cnt_rst(cnt_rst) );
 
 	datapath dp(.clk(clk), .rst(rst), .wr_en(write_en), .inreg_en(reg_en), .cnt_en(cnt_64_en),  
 			.cnt_co(counter_64_co), .cnt_value(cnt_value), .mem_line(line_in), .write_value(write_val), .cnt_rst(rst));
--- src/hdl/colparity.v
+++ src/hdl/colparity.v
@@ -7,16 +7,16 @@ module colParity (input1, input2, output_line);
     reg [24:0] temp;
 
     genvar i;
-    wire a_ijk = 1'b0;
-    wire parity1 = 1'b0;
-    wire parity2 = 1'b0;
+    wire [24:0] a_ijk;
+    wire [24:0] parity1;
+    wire [24:0] parity2;
 
     generate
         for(i = 0; i < 25; i = i + 1) begin
-            assign a_ijk = input2[i];
-            assign parity1 = input2[(i+14)%25] ^ input2[(i+19)%25] ^ input2[(i+24)%25] ^ input2[(i+4)%25] ^ input2[(i+9)%25];
-            assign parity2 = input1[(i+16)%25] ^ input1[(i+21)%25] ^ input1[(i+1)%25] ^ input1[(i+6)%25] ^ input1[(i+11)%25];
-            assign temp[i] = a_ijk ^ parity1 ^ parity2;
+            assign a_ijk[i] = input2[i];
+            assign parity1[i] = input2[(i+14)%25] ^ input2[(i+19)%25] ^ input2[(i+24)%25] ^ input2[(i+4)%25] ^ input2[(i+9)%25];
+            assign parity2[i] = input1[(i+16)%25] ^ input1[(i+21)%25] ^ input1[(i+1)%25] ^ input1[(i+6)%25] ^ input1[(i+11)%25];
+            assign temp[i] = a_ijk[i] ^ parity1[i] ^ parity2[i];
         end
     endgenerate
 
