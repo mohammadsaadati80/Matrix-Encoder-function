6c64dd7a65dd0b6ce9f49aa89ec01ef99e98af2d
mamadi80 <mm_araghi80@yahoo.com>
2023-01-02 18:07:23 +0330

    fix datapath

--- src/hdl/colparity.v
+++ src/hdl/colparity.v
@@ -1,26 +1,33 @@
-module colParity (input1, input2, swap_en, output_line);
+module colParity (input1, input2, output_line);
 
     input [24:0] input1;
     input [24:0] input2;
-    input swap_en;
     output reg [24:0] output_line;
 
     reg [24:0] temp;
 
-    integer i = 0;
+    genvar i = 0;
     integer x = 0;
     integer y = 0;
     integer new_x = 0;
     integer new_y = 0;
     integer new_pos = 0;
 
-    always @(swap_en) begin
-        if (swap_en) begin
-            for(i = 0; i < 25; i = i + 1) begin
-                // TODO
-            end
+    generate
+        for(i = 0; i < 25; i = i + 1) begin
+                x = i % 5;
+                y = i / 5;
+                x = (x + 3) % 5;
+                y = (y + 3) % 5;
+                
+                a = input2[x][y];
+
+
+
+                new_pos = (y * 5) + x;
+                temp[new_pos] = input_line[i];
         end
-    end
+    endgenerate
 
     assign output_line = temp;
 
--- src/hdl/datapath.v
+++ src/hdl/datapath.v
@@ -1,35 +1,26 @@
-module datapath (clk, rst, input_file_name, write_en, read_en, mux_en, reg_en, cnt_64_en, 
-				reg_rst, output_file_name, permute_en, counter_co, cnt_value, line_in, write_value);
+module datapath (clk, rst, wr_en, inreg_en, cnt_en, 
+				cnt_co, cnt_value, mem_line, write_value, cnt_rst);
 	
 	input clk;
 	input rst;
-	input [12*8-1:0] input_file_name;
-	input write_en;
-	input read_en;
-	input mux_en;
-	input reg_en;
-	input cnt_64_en;
-	input reg_rst; 
-	input [13*8-1:0] output_file_name;
-	input permute_en;
-	input [24:0] line_in;
-	output counter_co;
+	input wr_en;
+	input inreg_en;
+	input cnt_en;
+	input [24:0] mem_line;
+	input cnt_rst;
+	output cnt_co;
 	output [5:0] cnt_value;
 	output [24:0] write_value;
 	
-	wire [24:0] line;
 	wire [5:0] cnt_64_value;
-	wire [24:0] permutation_out;
+	wire [24:0] col_parity_out;
 	wire [24:0] reg_out;
-	wire [24:0] mux2to1_out;
-	assign cnt_value = cnt_64_value;
-	assign write_value = reg_out;
 
-	counter #(6) cnt1(.clk(clk), .en(cnt_64_en), .pin(cnt_64_value), .pout(cnt_64_value), .select(1'b1), .rst(rst), .ld(1'b0), .co(counter_co));
-	// read_from_file reader1(.clk(clk), .en(read_en),  .line_in(line_in), .line(line));
-	register reg1(.clk(clk),.pin(mux2to1_out),.en(reg_en),.rst(reg_rst),.pout(reg_out));
-	colParity colParity1(.input_line(reg_out), .swap_en(permute_en), .output_line(permutation_out));
-	// mux2to1 #(25) mux1 (.a(line),.b(permutation_out),.s(mux_en),.w(mux2to1_out));
-	//write_to_file write1(.output_file_name(output_file_name), .line(reg_out), .en(write_en));
+	assign cnt_value = cnt_64_value;
+	assign write_value = col_parity_out;
+
+	counter #(6) cnt1(.clk(clk), .en(cnt_en), .pin(cnt_64_value), .pout(cnt_64_value), .select(1'b1), .rst(cnt_rst), .ld(1'b0), .co(cnt_co));
+	register reg1(.clk(clk), .pin(mem_line), .en(inreg_en), .rst(rst), .pout(reg_out));
+	colParity colParity1(.input1(reg_out), .input2(mem_line), .output_line(col_parity_out));
 
 endmodule
